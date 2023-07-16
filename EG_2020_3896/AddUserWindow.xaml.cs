9e20f23f4324401b5d54bb9631791a0a11f7d990
Dilexsan <120619547+Dilexsan@users.noreply.github.com>
2023-07-16 19:07:36 +0530

    Add project files.

---
+++ EG_2020_3896/AddUserWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EG_2020_3896
+{
+   
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow(AddUserVM vm)
+
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ EG_2020_3896/MainWindowVM.cs
@@ -0,0 +1,98 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EG_2020_3896.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace EG_2020_3896
+{
+    public  partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public  ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser=null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+        [RelayCommand]
+        public void message()
+        {
+
+            MessageBox.Show("GPA value should be between 0 and 4.");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "Add Student";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student != null)
+            {
+                users.Add(vm.Student);
+               
+            }
+            
+           
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show(" Deleted successfuly. ");
+                
+            }
+            else
+            {
+                MessageBox.Show("Please Select the Student");
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "Edit Student";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student");
+            }
+        }
+
+        public  MainWindowVM()
+        {
+            users = new ObservableCollection<User>();  
+
+        }
+    }
+}
